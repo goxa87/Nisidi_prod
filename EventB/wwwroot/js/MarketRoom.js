ee41b043f218cbef8b2e574b10f2bb225c8108f2
Georgy Petrov <goxa87@rambler.ru>
2021-04-20 07:47:33 +0300

    refactor: убрал подменю из ЛК

--- EventB/wwwroot/js/MarketRoom.js
+++ EventB/wwwroot/js/MarketRoom.js
@@ -1,30 +1,5 @@
 ﻿$(document).ready(function ()
 {
-    // Переключение вкладок.
-    $('#lk-kibnet-selector').click(() => {
-        $('.lk-selector').removeClass('selected-selector');
-        $('#lk-kibnet-selector').addClass('selected-selector');
-        $('.lk-page').addClass('display-none');
-        $('#lk-kibnet-page').removeClass('display-none');
-    });
-    $('#lk-event-selector').click(() => {
-        $('.lk-selector').removeClass('selected-selector');
-        $('#lk-event-selector').addClass('selected-selector');
-        $('.lk-page').addClass('display-none');
-        $('#lk-event-page').removeClass('display-none');
-    });
-    $('#lk-cards-selector').click(() => {
-        $('.lk-selector').removeClass('selected-selector');
-        $('#lk-cards-selector').addClass('selected-selector');
-        $('.lk-page').addClass('display-none');
-        $('#lk-cards-page').removeClass('display-none');
-    });
-    $('#lk-banners-selector').click(() => {
-        $('.lk-selector').removeClass('selected-selector');
-        $('#lk-banners-selector').addClass('selected-selector');
-        $('.lk-page').addClass('display-none');
-        $('#lk-banners-page').removeClass('display-none');
-    });
     // Кнопка сменить статус.
     $('.right-columnn').on('click', '.lk-change-event-status', function () {
         let eveId = $(this).parent().children('.kibnet-event-id').val();
