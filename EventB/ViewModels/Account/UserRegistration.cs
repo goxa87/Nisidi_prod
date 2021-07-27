f68733dbef6b3833a48bb02e7423c541988742df
Georgy Petrov <goxa87@rambler.ru>
2021-07-27 21:24:48 +0300

    верстка и мелочи

--- EventB/ViewModels/Account/UserRegistration.cs
+++ EventB/ViewModels/Account/UserRegistration.cs
@@ -17,46 +17,46 @@ namespace EventB.ViewModels.Account
         [DataType(DataType.EmailAddress, ErrorMessage = "Неверный формат электронной почты")]
         public string Email { get; set; }
 
-        [Display(Name = "Придумате пароль не менее 6 символов")]
+        [Display(Name = "Пароль (не менее 6 символов)")]
         [Required(ErrorMessage = "Не введен пароль.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Display(Name = "Введите пароль еще раз")]
+        [Display(Name = "Пароль еще раз")]
         [Required(ErrorMessage = "Повторите пароль")]
         [DataType(DataType.Password), Compare(nameof(Password), ErrorMessage = "Пароли не совпадают")]
         public string СonfirmPassword { get; set; }
         /// <summary>
         /// Псевдоним
         /// </summary>
-        [Display(Name = "Ваше имя(будет видно другим пользователям)")]
+        [Display(Name = "Имя (будет видно другим пользователям)")]
         [Required(ErrorMessage = "Введите псевдоним"), MaxLength(200, ErrorMessage = "Максимальная длина 200 символов")]
         public string Name { get; set; }
         /// <summary>
         /// Фото на аватарку.
         /// </summary>
-        [Display(Name ="Ваша форография")]
+        [Display(Name = "Загрузить фото")]
         public IFormFile Photo { get; set; }
 
         /// <summary>
         /// город
         /// </summary>
         [Required(ErrorMessage="Введите город"), MaxLength(128,ErrorMessage ="Максимальная длина 128 символов")]
-        [Display(Name ="Ваш город(поиск и уведомления будут для этого города , будьте внимательны)")]
+        [Display(Name = "Город (поиск событий и уведомления для этого города)")]
         public string City { get; set; }
         /// <summary>
         /// интересы
         /// </summary>
         [MaxLength(1024,ErrorMessage ="Максимальная длина 1024 символа")]
-        [Display(Name ="Ваши интересы через запятую или пробел(по ним будем искать события, подходящие для вас)")]
+        [Display(Name = "Интересы (поможем подобрать подходящие события)")]
         public string Tegs { get; set; }
 
         [DataType(DataType.PhoneNumber)]
         [Display(Name = "Номер телефона (необязательно)")]
         public string PhoneNumber { get; set; }
 
-        [MaxLength(1000,ErrorMessage = "Максимальная длина 1000 символа")]
-        [Display(Name = "Информация о вас(до 1000 символов)")]
+        [MaxLength(1000,ErrorMessage = "Максимальная длина 1000 символов")]
+        [Display(Name = "О себе (до 1000 символов)")]
         public string Description { get; set; }
 
         /// <summary>
--- EventB/ViewModels/AddEventViewModel.cs
+++ EventB/ViewModels/AddEventViewModel.cs
@@ -14,7 +14,7 @@ namespace EventB.ViewModels
         [MaxLength(1000, ErrorMessage = "Длинна этого поля не должна быть больше 1000 символов")]
         public string Title { get; set; }
 
-        [Display(Name = "Теги тобытия")]
+        [Display(Name = "Теги")]
         [MaxLength(1000, ErrorMessage = "Длинна этого поля не должна быть больше 1000 символов")]
         public string Tegs { get; set; }
 
@@ -22,24 +22,25 @@ namespace EventB.ViewModels
         [MaxLength(4000, ErrorMessage = "Длинна этого поля не должна быть больше 4000 символов")]
         public string Body { get; set; }
 
-        [Required(ErrorMessage = "Поле Дата и время проведения не заполнено")]
-        [Display(Name = "Дата и время проведения события")]
+        [Required(ErrorMessage = "Поле Дата и время не заполнено")]
+        [Display(Name = "Дата и время")]
         public DateTime Date { get; set; }
 
-        [Display(Name = "Город проведения")]
-        [Required(ErrorMessage = "Поле город проведения не заполнено")]
+        [Display(Name = "Город")]
+        [Required(ErrorMessage = "Поле город не заполнено")]
         [MaxLength(100, ErrorMessage = "Длинна этого поля не должна быть больше 100 символов")]
         public string City { get; set; }
 
-        [Display(Name = "Место проведения")]
+        [Display(Name = "Место")]
         [Required(ErrorMessage = "Поле Место проведения не заполнено")]
         [MaxLength(1000, ErrorMessage = "Длинна этого поля не должна быть больше 200 символов")]
         public string Place { get; set; }
 
-        [MaxLength(1000, ErrorMessage = "Слишком длинное, попробуйте до 1000 символов"), Display(Name = "Если нужны билеты, опишите что и как")]
+        [MaxLength(1000, ErrorMessage = "Слишком длинное, попробуйте до 1000 символов"),
+            Display(Name = "Билеты. Где и как купить?")]
         public string TicketsDesc { get; set; }
 
-        [MaxLength(25), Display(Name ="Телефон для связи (НЕОБЯЗАТЕЛЬНО)")]
+        [MaxLength(25), Display(Name ="Телефон для связи (Необязательно)")]
         public string Phone { get; set; }
 
         [Display(Name = "Возрастные ограничения мероприятия (Обязательно для публичных мероприятий)")]
