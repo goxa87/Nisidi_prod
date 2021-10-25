a0018c23256417466c8def4b1ef7061e07f98023
Georgy Petrov <goxa87@rambler.ru>
2021-10-25 07:45:28 +0300

    feat: хелпер строковое представление даты

---
+++ CommonServices/Infrastructure/Helpers/DateHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonServices.Infrastructure.Helpers
+{
+    public static class DateHelper
+    {
+        /// <summary>
+        /// Привести значение к формату (DDseparatorMMseparatorYYYY)
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="numericmonth">Использовать числовой месяц(1) строковый(0)</param>
+        /// <param name="separator">То чем будем разделять</param>
+        /// <param name="addYear">Добавлять ли год</param>
+        /// <returns></returns>
+        public static string ToDateStringmonth(this DateTime date, bool numericmonth = true, string separator = ".", bool addYear = true)
+        {
+            var months = new[]
+            {
+                "января",
+                "февраля",
+                "марта",
+                "апреля",
+                "мая",
+                "июня",
+                "июля",
+                "августа",
+                "сентября",
+                "октября",
+                "ноября",
+                "декабря",
+            };
+            var Day = date.Day.ToString();
+            var Month = numericmonth ? date.Month.ToString() : months[date.Month - 1];
+            var Year = addYear ? $"{separator}{date.Year.ToString()}" : string.Empty;
+            var result = $"{Day}{separator}{Month}{Year}";
+            return result;
+        }
+    }
+}
---
+++ EventBTests/StringHelperTest.cs
@@ -0,0 +1,36 @@
+﻿using CommonServices.Infrastructure.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBTests
+{
+    [TestClass]
+    public class StringHelperTest
+    {
+        [TestMethod]
+        public void ToDateStringmonth_FormattedForCard()
+        {
+            var input = new DateTime(2021, 11, 10, 14, 45, 55);
+            string output =  "10 ноября";
+
+            var rezult = input.ToDateStringmonth(false," ", false);
+
+            Assert.AreEqual(output, rezult);
+        }
+
+        [TestMethod]
+        public void ToDateStringmonth_FormattedForCard2()
+        {
+            var input = new DateTime(2021, 11, 10);
+            string output = "10.11.2021";
+
+            var rezult = input.ToDateStringmonth(true, ".", true);
+
+            Assert.AreEqual(output, rezult);
+        }
+    }
+}
